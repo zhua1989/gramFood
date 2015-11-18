85cec42d3918fd896fb32eec62d10d559d67dcf4
Anthony Zhu <zhua89@gmail.com>
2015-11-17 19:46:56 -0500

    got random post to display

--- app/assets/javascripts/app.js
+++ app/assets/javascripts/app.js
@@ -1,12 +1,14 @@
-var gramApp = {models: {}, collections: {}, views: {}}
-$(document).ready(function(){
-  console.log('this is linked');
-  gramApp.initialize();
-});
+// var gramApp = {models: {}, collections: {}, views: {}}
+// $(document).ready(function(){
+//   console.log('this is linked');
+//   gramApp.initialize();
+// });
 
-gramApp.initialize = function(){
-  var restaurantCollection = new gramApp.collections.restaurantCollection({});
-  var restaurantCollectionView = new gramApp.views.restaurantCollectionView({
-    collection: restaurantCollection
-  });
-}
+// gramApp.initialize = function(){
+//   var restaurantCollection = new gramApp.collections.restaurantCollection({});
+//   var restaurantCollectionView = new gramApp.views.restaurantCollectionView({
+//     collection: restaurantCollection
+//   });
+// }
+
+
--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,8 +1,30 @@
+require 'httparty'
+require 'pry'
+
 class WelcomeController < ApplicationController
 
 
 def index
   
+  geo_request = HTTParty.get('http://ipinfo.io/geo/')
+  location = geo_request["loc"]
+  splitted = location.split(",")
+  
+  # read the api_key from local_env.yml file
+  apiKey = ENV["instagram_key"]
+  @APIurl = "https://api.instagram.com/v1/tags/foodporn/media/recent?client_id=" + apiKey
+  #hit API for Results
+  request = HTTParty.get(@APIurl)
+  #get random post and get image url 
+  @random_post = request["data"].sample
+  @random_image = @random_post["images"]["standard_resolution"]["url"]
+  @random_captions = @random_post["caption"]["text"]
+  puts @random_image
+  puts @random_captions
+
+
+
+
 end
 
 
